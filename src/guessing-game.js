8e593985352022417c0c5330f9126ad028eb19bc
MINSK\Yauheni_Ralko <yauheni_ralko@epam.com>
2017-09-20 11:19:09 +0300

    Guessing Game has been successfully created

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.max = 0;
+      this.min = 0;
+      this.pivot = 0;
+
+    }
 
     setRange(min, max) {
+       this.max = max;
+       this.min = min;
 
     }
 
     guess() {
+      return this.pivot = Math.round((this.max - this.min) / 2) + this.min;
 
-    }
+      }
 
     lower() {
-
+      this.max = this.pivot;
     }
 
     greater() {
-
+      this.min = this.pivot;
     }
 }
 
